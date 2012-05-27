1de23afba004c7258dbefb6f277403ef90733d16
Jesse Bounds <jesse@rebounds.net>
2012-05-26 17:50:14 -0700

    fully dynamic view query and row object dot notation

--- tests.py
+++ tests.py
@@ -37,8 +37,35 @@ class CoreTests(unittest.TestCase):
         mock_adapter = Mock()
         mock_adapter.find_by_id.return_value = self.test_view_data
         windy = WindyPie(mock_adapter)
+        # get a specific view
         view = windy.views('z8bn-74gv')
+        # counts of "raw" socrata data are as expected
         self.assertEqual(len(view.rows), 24)
+        self.assertEqual(len(view.columns), 18)
+        # the "WindyPie" version of the data, formatted in a list of python dicts
+        self.assertEqual(len(view.collection), 24)
+        expected_fields = ['sid', 'id', 'position', 'created_at', \
+                           'created_meta', 'updated_at', 'updated_meta', \
+                           'meta', 'district', 'address', 'city', \
+                           'state', 'zip', 'website', 'phone', \
+                           'fax', 'tty', 'location']
+        # make sure it has the right fields
+        self.assertEqual(view.fields, expected_fields)
+        # filter the collection on a specific id
+        id_collection = view.collection_by_id('65FCCC12-E3B1-4BB8-8584-71A815E14289')
+        self.assertEqual(len(id_collection), 1)
+        self.assertEqual(id_collection[0].id, '65FCCC12-E3B1-4BB8-8584-71A815E14289')
+        # we can filter on any field (it's dynamic!), let's try position
+        position_collection = view.collection_by_position('6')
+        self.assertEqual(len(position_collection), 1)
+        self.assertEqual(position_collection[0].position, '6')
+        # and address
+        address_collection = view.collection_by_address('5701 W Madison St')
+        self.assertEqual(len(address_collection), 1)
+        self.assertEqual(address_collection[0].address, '5701 W Madison St')
+        # and, for good measure, let's filter by zip to see if more than one row has the same value
+        zip_collection = view.collection_by_zip('60630')
+        self.assertEqual(len(zip_collection), 2) # (there are 2 with the same zip)
 
     def test_get_views_by_name(self):
         '''should return all views with view_name in the name field'''
