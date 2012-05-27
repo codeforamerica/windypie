1ee8a4bb5c15f164b0154e27a2aec3abf2262980
Jesse Bounds <jesse@rebounds.net>
2012-05-27 09:28:57 -0700

    fixed the build. phew

--- tests.py
+++ tests.py
@@ -42,7 +42,7 @@ class CoreTests(unittest.TestCase):
         view = windy.views('z8bn-74gv')
         # counts of "raw" socrata data are as expected
         self.assertEqual(len(view.rows), 24)
-        self.assertEqual(len(view.columns), 19)
+        self.assertEqual(len(view.columns), 18)
         # the "WindyPie" version of the data, formatted in a list of python dicts
         self.assertEqual(len(view.collection), 24)
         expected_fields = ['sid', 'id', 'position', 'created_at', \
@@ -91,6 +91,7 @@ class CoreTests(unittest.TestCase):
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(CoreTests)
     result = unittest.TextTestRunner(verbosity=2).run(suite)
+    # this is not required but allows for custom exit codes and analysis fo failures
     if len(result.failures) > 0:
         print 'falures count %s' % len(result.failures)
         sys.exit(1)
